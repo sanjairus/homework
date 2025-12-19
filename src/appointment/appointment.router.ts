1aecd8c9a945a35750795a0bd5a11cce8ef26be0
Jairus San Juan <san.jairus@gmail.com>
2025-12-19 18:58:01 +0800

    chore: Wrap route handlers with try-catch (appointment.router)

--- src/appointment/appointment.router.ts
+++ src/appointment/appointment.router.ts
@@ -3,6 +3,7 @@ import { DataSource } from "typeorm";
 import AppointmentService from "@appointment/appointment.service";
 import { Request, Response } from "express";
 import { CreateAppointmentParams } from "./appointment.type";
+import { HttpError } from "@core/error";
 
 class AppointmenRouter extends BaseRouter {
   datasource: DataSource;
@@ -20,49 +21,73 @@ class AppointmenRouter extends BaseRouter {
   }
 
   public getAppointments = async (request: Request, response: Response) => {
-    console.log("Start of AppointmentRouter: getAppointments");
+    try {
+      console.log("Start of AppointmentRouter: getAppointments");
 
-    const appointments = await this.service.findAppointments();
+      const appointments = await this.service.findAppointments();
 
-    console.log("End of AppointmentRouter: getAppointments");
-    return response.status(200).json({ appointments });
+      console.log("End of AppointmentRouter: getAppointments");
+      return response.status(200).json({ appointments });
+    } catch (error: unknown) {
+      if (error instanceof HttpError) {
+        const code = error.code;
+        const message = error.message;
+        response.status(code).json({ message });
+      }
+    }
   };
 
   public createAppointment = async (request: Request, response: Response) => {
-    console.log("Start of AppointmentRouter: createAppointment");
+    try {
+      console.log("Start of AppointmentRouter: createAppointment");
 
-    const rawSchedule = request.body.schedule || new Date().toUTCString();
+      const rawSchedule = request.body.schedule || new Date().toUTCString();
 
-    const schedule = new Date(rawSchedule);
+      const schedule = new Date(rawSchedule);
 
-    const customerEmail = request.body.customerEmail;
-    const customerPhone = request.body.customerPhone;
-    const customerName = request.body.customerName;
+      const customerEmail = request.body.customerEmail;
+      const customerPhone = request.body.customerPhone;
+      const customerName = request.body.customerName;
 
-    const params: CreateAppointmentParams = {
-      schedule,
-      customerEmail,
-      customerName,
-      customerPhone,
-    };
+      const params: CreateAppointmentParams = {
+        schedule,
+        customerEmail,
+        customerName,
+        customerPhone,
+      };
 
-    const appointment = await this.service.createAppointment(params);
+      const appointment = await this.service.createAppointment(params);
 
-    console.log("End of AppointmentRouter: createAppointment");
-    return response.status(200).json({ appointment });
+      console.log("End of AppointmentRouter: createAppointment");
+      return response.status(200).json({ appointment });
+    } catch (error: unknown) {
+      if (error instanceof HttpError) {
+        const code = error.code;
+        const message = error.message;
+        response.status(code).json({ message });
+      }
+    }
   };
 
   public validateAppointment = async (request: Request, response: Response) => {
-    console.log("Start of AppointmentRouter: validateAppointment");
+    try {
+      console.log("Start of AppointmentRouter: validateAppointment");
 
-    const pin = request.body.pin;
+      const pin = request.body.pin;
 
-    const appointment = await this.service.validateAppointment(pin);
+      const appointment = await this.service.validateAppointment(pin);
 
-    console.log("End of AppointmentRouter: validateAppointment");
-    return response.status(200).json({
-      appointment,
-    });
+      console.log("End of AppointmentRouter: validateAppointment");
+      return response.status(200).json({
+        appointment,
+      });
+    } catch (error: unknown) {
+      if (error instanceof HttpError) {
+        const code = error.code;
+        const message = error.message;
+        response.status(code).json({ message });
+      }
+    }
   };
 }
 
