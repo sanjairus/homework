f935dd4e9d48586ba89eb9a2e9a488189131b083
Jairus San Juan <san.jairus@gmail.com>
2025-12-19 16:46:17 +0800

    chore: Add calculateMillisExtension and validateAppointment method (appointment.service)

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -46,6 +46,105 @@ class AppointmentService {
     console.log("End of AppointmentService: createAppointment");
     return appointment;
   }
+
+  private calculateMillisExtension(peopleAhead: number) {
+    console.log("Start of AppointmentService: calculateTimeExtension");
+
+    const baseMinutes: number = 5;
+    const growthRate: number = 1.3;
+    const maxMinutes: number = 60;
+
+    if (peopleAhead < 0) {
+      throw new Error("Num people ahead should be >= 0");
+    }
+
+    if (peopleAhead === 0) {
+      return 0;
+    }
+
+    const MS_PER_SECOND = 1000;
+    const SECONDS_PER_MINUTE = 60;
+    const MS_PER_MINUTE = MS_PER_SECOND * SECONDS_PER_MINUTE;
+
+    const baseMs = MS_PER_MINUTE * baseMinutes;
+    const maxMs = MS_PER_MINUTE * maxMinutes;
+
+    const extension = baseMs * Math.pow(growthRate, peopleAhead);
+
+    console.log("End of AppointmentService: calculateTimeExtension");
+
+    return Math.min(extension, maxMs);
+  }
+
+  async validateAppointment(pin: string) {
+    console.log("Start of AppointmentService: validateAppointment");
+
+    const appointments = await this.appointmentRepository.find({
+      where: {
+        schedule: {
+          $gt: new Date(),
+        },
+      },
+      order: {
+        schedule: 1,
+      },
+    });
+
+    const position = appointments.findIndex((e) => e.pin === pin);
+    const appointment = appointments[position];
+
+    if (!appointment) {
+      throw new Error("Invalid Appointment");
+    }
+
+    const extension = this.calculateMillisExtension(position);
+
+    const schedule = appointment.schedule;
+    const time = schedule.getTime();
+
+    // Requirement: Allow pin usage 15 minutes prior
+    const MS_IN_FIFTEEN_MINUTES = 1000 * 60 * 15;
+    const validFrom = new Date(time - MS_IN_FIFTEEN_MINUTES);
+
+    // Calculate pin extended validity (no db operations needed)
+    const validUntil = new Date(time + extension);
+
+    const now = new Date();
+
+    // check if within range
+    const isValid = now >= validFrom && now <= validUntil;
+
+    console.log({
+      originalSchedule: schedule.toLocaleString(),
+      currentTime: now.toLocaleString(),
+      validFrom: validFrom.toLocaleString(),
+      validUntil: validUntil.toLocaleString(),
+      isValid,
+    });
+
+    console.log({
+      appointments,
+      position,
+      extensionInMinutes: extension / 1000 / 60,
+    });
+
+    if (!isValid) {
+      throw new Error(`Appointment with pin:${pin} is invalid`);
+    }
+
+    const updatedDoc = await this.appointmentRepository.findOneAndUpdate(
+      { pin },
+      {
+        $set: {
+          isConfirmed: true,
+        },
+      },
+      { returnDocument: "after" }
+    );
+
+    console.log("End of AppointmentService: validateAppointment");
+    return updatedDoc;
+  }
 }
 
 export default AppointmentService;
