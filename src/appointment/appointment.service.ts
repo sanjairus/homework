969794c69c5925a780949ac35f83d9fb81fd2224
Jairus San Juan <san.jairus@gmail.com>
2025-12-19 16:26:33 +0800

    chore: Add createAppointment method (appointment.service)

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -1,20 +1,22 @@
-import { DataSource } from "typeorm";
+import { DataSource, MongoRepository } from "typeorm";
 import { Appointment } from "@appointment/appointment.entity";
+import { randomInt } from "node:crypto";
 
 class AppointmentService {
   datasource: DataSource;
+  appointmentRepository: MongoRepository<Appointment>;
 
   constructor(datasource: DataSource) {
     this.datasource = datasource;
+
+    this.appointmentRepository =
+      this.datasource.getMongoRepository(Appointment);
   }
 
   async findAppointments() {
     console.log("Start of AppointmentService: findAppointments");
 
-    const appointmentRepository =
-      this.datasource.getMongoRepository(Appointment);
-
-    const appointments = await appointmentRepository.find({
+    const appointments = await this.appointmentRepository.find({
       order: {
         schedule: 1,
       },
@@ -22,6 +24,27 @@ class AppointmentService {
 
     console.log("End of AppointmentService: findAppointments");
     return appointments;
+  }
+
+  async createAppointment(schedule: Date) {
+    console.log("Start of AppointmentService: createAppointment");
+
+    const appointment = new Appointment();
+
+    const generatePin = () => {
+      const num = randomInt(0, 10000);
+
+      return num.toString().padStart(4, "0");
+    };
+
+    appointment.pin = generatePin();
+    appointment.schedule = schedule;
+    appointment.isConfirmed = false;
+
+    await this.appointmentRepository.save(appointment);
+
+    console.log("End of AppointmentService: createAppointment");
+    return appointment;
   }
 }
 
